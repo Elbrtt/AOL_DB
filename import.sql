a776461ebd7f618dc8e1196d024b65b42993275b
Jordhy <alexanderwibisonoj@gmail.com>
2026-01-11 18:22:26 +0700

    create holding table

--- import.sql
+++ import.sql
@@ -1,3 +1,14 @@
+CREATE TABLE online_retail (
+    Invoice VARCHAR(20),
+    StockCode VARCHAR(20),
+    Description TEXT,
+    Quantity INT,
+    InvoiceDate DATETIME,
+    Price DECIMAL(10, 2),
+    CustomerID INT,
+    Country VARCHAR(50)
+);
+
 LOAD DATA INFILE 'online_retail_data.csv' 
 INTO TABLE online_retail 
 FIELDS TERMINATED BY ',' 
